93f43290baa973841e08437140059a77d9a5f187
Lyz03 <lizoe@netcourrier.com>
2021-11-15 14:11:15 +0100

    part 1

---
+++ exo 5/app.js
@@ -0,0 +1,6 @@
+let numberOne = prompt('entrer un nombre à virgule :');
+let numberTwo = prompt('entrer un autre nombre à virgule :');
+
+result = parseInt(numberOne) * parseInt(numberTwo);
+
+alert(result);
